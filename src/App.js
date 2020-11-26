3bb2c23391e18ab915d572c2e79daf087d484a08
gilbertzhuo <gilbertzhuo@gmail.com>
2020-11-26 18:38:08 +1100

    AI photo generator

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [image, setImage] = useState('')
+  const handleChange = () => {
+    axios.get('https://api.generated.photos/api/v1/faces?api_key=ZYQVnE5FE-NFyOciA4ggeQ&order_by=random')
+    .then(res => {
+      const uri = res.data.faces[0]["urls"][4]["512"];
+      uri && setImage(uri)
+    }).catch(err=>{
+      console.log(err.message);
+    });
+  }
+  return (
+    <div className="App">
+     <h1>AI Photo Generator</h1>
+     {image && <img src={image} alt="AI Face"/>}
+     <button type ='button' onClick={handleChange}>New Image</button>
+    </div>
+  );
+}
+
+export default App;
+
+
